0dd2475bc358be7b7542280079d701f3d64e337e
SeppeP <seppe.parys8@gmail.com>
2025-09-08 11:12:42 +0200

    ProfileCard added

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,8 +1,21 @@
 <template>
-
+  <ProfileCard/>
 </template>
 
 <script setup lang="ts">
+import ProfileCard from "./ProfileCard.vue";
+import {onMounted} from "vue";
+import useProfileStore from "../stores/ProfileStore.ts";
+import useAchievementStore from "../stores/AchievementStore.ts";
+
+onMounted(() => {
+  const profileStore = useProfileStore();
+  const achievementStore = useAchievementStore();
+
+  profileStore.getProfile();
+  achievementStore.getAchievements();
+})
+
 </script>
 
 <style>
--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,11 +1,31 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-
+  <div v-if="profile" class="profile">
+    <img :src="profile.avatar_url" :alt="`${profile.name}'s profile picture`">
+    <h1 class="profile__name">{{ profile.name }}</h1>
+    <p class="profile__level">Level {{ profile.level }}</p>
+  </div>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+import useProfileStore from "../stores/ProfileStore.ts";
+import {storeToRefs} from "pinia";
 
+const {profile} = storeToRefs(useProfileStore());
+</script>
+
+<style lang="scss">
+  .profile {
+    margin: 4.5rem auto 0;
+    max-width: fit-content;
+    text-align: center;
+
+    &__name {
+      font-size: 1.5rem;
+      margin-top: .75rem;
+    }
+
+    &__level {
+      font-size: .75rem;
+    }
+  }
 </style>
