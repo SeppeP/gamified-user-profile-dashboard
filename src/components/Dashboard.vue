94b4aa8e5196970e5a219f0f2b892a31679c005b
SeppeP <seppe.parys8@gmail.com>
2025-09-08 13:11:40 +0200

    loader visual added

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="dashboard">
+  <div v-if="profile && achievements" class="dashboard">
     <ProfileCard class="dashboard__profile-card"/>
     <LevelProgress :current_xp="profile?.current_xp" :max_xp="profile?.xp_to_next_level"
                    class="dashboard__levelProgress"/>
     <AchievementList/>
   </div>
+  <Loader v-else/>
 </template>
 
 <script setup lang="ts">
@@ -15,6 +16,7 @@ import useAchievementStore from "../stores/AchievementStore.ts";
 import LevelProgress from "@/components/LevelProgress.vue";
 import {storeToRefs} from "pinia";
 import AchievementList from "@/components/AchievementList.vue";
+import Loader from "@/components/loader.vue";
 
 onMounted(() => {
   const profileStore = useProfileStore();
@@ -24,21 +26,22 @@ onMounted(() => {
   achievementStore.getAchievements();
 })
 
-const { profile } = storeToRefs(useProfileStore());
+const {profile} = storeToRefs(useProfileStore());
+const {achievements} = storeToRefs(useAchievementStore());
 
 </script>
 
 <style lang="scss">
- .dashboard {
-   margin: 1rem;
+.dashboard {
+  margin: 1rem;
 
-   &__profile-card {
-     margin-bottom: 1rem;
-   }
+  &__profile-card {
+    margin-bottom: 1rem;
+  }
 
-   &__levelProgress {
-     max-width: 30rem;
-     margin: 0 auto 1rem;
-   }
- }
+  &__levelProgress {
+    max-width: 30rem;
+    margin: 0 auto 1rem;
+  }
+}
 </style>
---
+++ src/components/loader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="loader">
+    <p>Loading...</p>
+  </div>
+</template>
+
+<style lang="scss">
+.loader {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
